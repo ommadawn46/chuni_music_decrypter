8a679b8afe66d948b7dbdaecce6e38959e6fbb40
ommadawn46 <ommadawn46@gmail.com>
2018-08-08 17:55:33 +0900

    Add methods to wrap key management

--- chuni_music_decrypter.py
+++ chuni_music_decrypter.py
@@ -30,10 +30,10 @@ def crypto(mode, text, key, iv, isURL):
     out, err = p.communicate()
     return out, err
 
-def encode(text, key, iv, isURL):
+def encrypt(text, key, iv, isURL):
     return crypto('-e', text, key, iv, isURL)
 
-def decode(text, key, iv, isURL):
+def decrypt(text, key, iv, isURL):
     return crypto('-d', text, key, iv, isURL)
 
 def hmac_sign(m):
@@ -56,27 +56,27 @@ class BurpExtender(IBurpExtender, IHttpListener, IMessageEditorTabFactory):
         self.key = INIT_KEY
         self.iv = INIT_IV
 
-        self.request_keys = {}
-        self.response_keys = {}
+        self.keystore = {}
 
     # implement IHttpListener
-    # リクエスト・レスポンスを鍵とした暗号鍵・IVの辞書を作る
+    # リクエスト・レスポンスをキーとした暗号鍵・IVの辞書を作る
     def processHttpMessage(self, toolFlag, isRequest, messageInfo):
         if not isRequest:
             response = messageInfo.getResponse()
             request = messageInfo.getRequest()
             responseBody = self.extractBody(response, isRequest)
 
-            self.request_keys[request.tostring()] = (self.key, self.iv)
-            self.response_keys[response.tostring()] = (self.key, self.iv)
+            # 暗号鍵・IVの登録
+            self.storeKey(request, self.key, self.iv)
+            self.storeKey(response, self.key, self.iv)
 
-            out, err = decode(responseBody.tostring(), self.key, self.iv, '1')
+            out, err = decrypt(responseBody.tostring(), self.key, self.iv, '1')
             if not self.updateKey(out):
                 # 鍵が合わない場合は初期鍵も試す
-                out, err = decode(responseBody.tostring(), INIT_KEY, INIT_IV, '1')
+                out, err = decrypt(responseBody.tostring(), INIT_KEY, INIT_IV, '1')
                 if self.updateKey(out):
-                    self.request_keys[request.tostring()] = (INIT_KEY, INIT_IV)
-                    self.response_keys[response.tostring()] = (INIT_KEY, INIT_IV)
+                    self.storeKey(request, INIT_KEY, INIT_IV)
+                    self.storeKey(response, INIT_KEY, INIT_IV)
 
     # implement IMessageEditorTabFactory
     def createNewInstance(self, controller, editable):
@@ -95,6 +95,22 @@ class BurpExtender(IBurpExtender, IHttpListener, IMessageEditorTabFactory):
         except:
             return False
         return True
+
+    def storeKey(self, content, key, IV):
+        '''リクエスト・レスポンスのハッシュ値をキーとして暗号鍵・IVを辞書に登録する'''
+        digest = hashlib.md5(content.tostring()).hexdigest()
+        self.keystore[digest] = (key, IV)
+
+    def getKey(self, content):
+        '''リクエスト・レスポンスに対応する鍵を取得する'''
+        digest = hashlib.md5(content.tostring()).hexdigest()
+        if digest in self.keystore:
+            return self.keystore[digest]
+
+    def keyExists(self, content):
+        '''リクエスト・レスポンスに対応する暗号鍵が存在するか'''
+        digest = hashlib.md5(content.tostring()).hexdigest()
+        return digest in self.keystore
 
     def extractBody(self, content, isRequest):
         '''array型のrequest/responseからボディを抽出する'''
@@ -153,20 +169,19 @@ class ChuniMusicInputTab(IMessageEditorTab):
             self._txtInput.setText(None)
             self._txtInput.setEditable(False)
         else:
-            key, iv = self._extender.key, self._extender.iv
-            if isRequest:
-                # リクエストボディの復号
-                if content.tostring() in self._extender.request_keys:
-                    key, iv = self._extender.request_keys[content.tostring()]
-                parameter = self._extender._helpers.getRequestParameter(content, "data")
-                message = parameter.getValue()
+            # 暗号鍵の取得
+            if self._extender.keyExists(content):
+                key, iv = self._extender.getKey(content)
             else:
-                # レスポンスボディの復号
-                if content.tostring() in self._extender.response_keys:
-                    key, iv = self._extender.response_keys[content.tostring()]
-                message = self._extender.extractBody(content, isRequest).tostring()
-            out, err = decode(message, key, iv, '1')
-            self._txtInput.setText(out)
+                key, iv = self._extender.key, self._extender.iv
+            # 暗号文の取得
+            if isRequest:
+                encrypted = self._extender._helpers.getRequestParameter(content, "data").getValue()
+            else:
+                encrypted = self._extender.extractBody(content, isRequest).tostring()
+            # 復号してタブに表示
+            plain, err = decrypt(encrypted, key, iv, '1')
+            self._txtInput.setText(plain)
             self._txtInput.setEditable(self._editable)
 
         self._currentMessage = content
@@ -174,34 +189,41 @@ class ChuniMusicInputTab(IMessageEditorTab):
 
     # DecryptedDataタブで編集したメッセージを再暗号化する
     def getMessage(self):
+        content, isRequest = self._currentMessage, self._currentIsRequest
         if self._txtInput.isTextModified():
-            message = self._txtInput.getText().tostring()
-            key, iv = self._extender.key, self._extender.iv
-            if self._currentIsRequest:
-                # リクエストボディの暗号化
-                if self._currentMessage.tostring() in self._extender.request_keys:
-                    key, iv = self._extender.request_keys[self._currentMessage.tostring()]
-                out, err = encode(message, key, iv, '1')
-                content = self._extender._helpers.updateParameter(self._currentMessage, self._extender._helpers.buildParameter("data", out, IParameter.PARAM_BODY))
+            # 暗号鍵の取得
+            if self._extender.keyExists(content):
+                key, iv = self._extender.getKey(content)
             else:
-                # レスポンスボディの暗号化
-                if self._currentMessage.tostring() in self._extender.response_keys:
-                    key, iv = self._extender.response_keys[self._currentMessage.tostring()]
-                headersArray = self._extender.extractHeaders(self._currentMessage, self._currentIsRequest)
-                out, err = encode(message, key, iv, '0')
-                content = self._extender._helpers.buildHttpMessage(headersArray, array.array('b', out))
+                key, iv = self._extender.key, self._extender.iv
+            # 編集済みメッセージの取得
+            plain = self._txtInput.getText().tostring()
 
-            headersArray = self._extender.extractHeaders(content, self._currentIsRequest)
-            body = self._extender.extractBody(content, self._currentIsRequest)
+            if isRequest:
+                # 暗号化してリクエストボディに追加
+                encrypted, err = encrypt(plain, key, iv, '1')
+                data_param = self._extender._helpers.buildParameter("data", encrypted, IParameter.PARAM_BODY)
+                content = self._extender._helpers.updateParameter(content, data_param)
+            else:
+                # 暗号化してレスポンスボディに追加
+                encrypted, err = encrypt(plain, key, iv, '0')
+                headersArray = self._extender.extractHeaders(content, isRequest)
+                content = self._extender._helpers.buildHttpMessage(headersArray, array.array('b', encrypted))
 
             # ボディを再署名し、X-Signatureを書き換える
+            headersArray = self._extender.extractHeaders(content, isRequest)
             for i in range(len(headersArray)):
                 if 'X-Signature' in headersArray[i]:
-                    headersArray[i] = 'X-Signature: ' + hmac_sign(message)
+                    headersArray[i] = 'X-Signature: ' + hmac_sign(plain)
                     break
-            return self._extender._helpers.buildHttpMessage(headersArray, body)
+            body = self._extender.extractBody(content, isRequest)
+            newContent = self._extender._helpers.buildHttpMessage(headersArray, body)
+
+            # 暗号鍵・IVの登録
+            self._extender.storeKey(newContent, key, iv)
+            return newContent
         else:
-            return self._currentMessage
+            return content
 
     def isModified(self):
         return self._txtInput.isTextModified()
